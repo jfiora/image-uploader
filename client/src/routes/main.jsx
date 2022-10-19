9328aad72ee68e85c717412b2d69a259a27ee5f6
jfiora <sillonpeludo@gmail.com>
2022-10-19 11:40:49 -0300

    move routes

--- client/src/routes/main.jsx
+++ client/src/routes/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import UploaderPage from './pages/Uploader/UploaderPage';
-import HomePage from './pages/Home/HomePage';
-import NotFoundPage from './pages/404/NotFoundPage';
-import './styles/index.css';
+import UploaderPage from '../pages/Uploader/UploaderPage';
+import HomePage from '../pages/Home/HomePage';
+import NotFoundPage from '../pages/404/NotFoundPage';
+import '../styles/index.css';
 import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 
 const router = createBrowserRouter([
