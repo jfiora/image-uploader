4d52fcd3cebc85f34f2088c432f2e0a616712538
jfiora <sillonpeludo@gmail.com>
2022-11-30 11:33:07 -0300

    finished uploader and animation

--- client/src/components/Uploader/Uploader.jsx
+++ client/src/components/Uploader/Uploader.jsx
@@ -6,6 +6,7 @@ import image from '../../assets/image.svg';
 const Uploader = () => {
     const uploadUrl = import.meta.env.VITE_UPLOAD_URL;
     const [file, setFile] = useState();
+    const [uploaded, setUploaded] = useState(false);
     const { getRootProps, getInputProps } = useDropzone({
         accept: 'image/*',
         onDrop: (acceptedFiles) => {
@@ -19,59 +20,71 @@ const Uploader = () => {
         const data = new FormData();
         data.append('file', file);
         postImage(uploadUrl, data);
+        setUploaded(true);
     };
 
-    return (
-        <div className='Uploader-container'>
-            <h1 className='Uploader-title separator'>Upload your image</h1>
-            <form className='Uploader-form' action='#'>
-                <span className='Uploader-form-label separator' htmlFor='file'>
-                    File should be Jpeg, Png...
-                </span>
-
-                <div
-                    className='Uploader-dad-container separator'
-                    {...getRootProps()}
-                >
-                    <img
-                        src={image}
-                        className='Uploader-input-image'
-                        alt='dnd'
-                    />
-                    <input
-                        {...getInputProps()}
-                        type='file'
-                        id='file'
-                        accept='.jpg'
-                    />{' '}
-                    <p className='Uploader-input-text'>
-                        Drag & Drop your image here
-                    </p>
-                </div>
-
-                <span className='Uploader-or separator'>Or</span>
-                <div className='Uploader-cs-container separator'>
-                    <input
-                        className='Uploader-input'
-                        type='file'
-                        id='file'
-                        accept='.jpg'
-                        onChange={(e) => {
-                            const image = e.target.files[0];
-                            setFile(image);
-                            send();
-                        }}
-                    />
-                    <label
-                        className='Uploader-input-label separator'
+    {
+        return !uploaded ? (
+            <div className='Uploader-container'>
+                <h1 className='Uploader-title separator'>Upload your image</h1>
+                <form className='Uploader-form' action='#'>
+                    <span
+                        className='Uploader-form-label separator'
                         htmlFor='file'
                     >
-                        Choose a file
-                    </label>
-                </div>
-            </form>
-        </div>
-    );
+                        File should be Jpeg, Png...
+                    </span>
+
+                    <div
+                        className='Uploader-dad-container separator'
+                        {...getRootProps()}
+                    >
+                        <img
+                            src={image}
+                            className='Uploader-input-image'
+                            alt='dnd'
+                        />
+                        <input
+                            {...getInputProps()}
+                            type='file'
+                            id='file'
+                            accept='.jpg'
+                        />{' '}
+                        <p className='Uploader-input-text'>
+                            Drag & Drop your image here
+                        </p>
+                    </div>
+
+                    <span className='Uploader-or separator'>Or</span>
+                    <div className='Uploader-cs-container separator'>
+                        <input
+                            className='Uploader-input'
+                            type='file'
+                            id='file'
+                            accept='.jpg'
+                            onChange={(e) => {
+                                const image = e.target.files[0];
+                                setFile(image);
+                                send();
+                            }}
+                        />
+                        <label
+                            className='Uploader-input-label separator'
+                            htmlFor='file'
+                        >
+                            Choose a file
+                        </label>
+                    </div>
+                </form>
+            </div>
+        ) : (
+            <div className='Uploader-container loading'>
+                {' '}
+                <span>Uploading...</span>
+                <div className='progress-bar'></div>
+            </div>
+        );
+    }
 };
 
 export default Uploader;
